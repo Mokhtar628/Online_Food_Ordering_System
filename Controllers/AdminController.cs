fb6d724ae3f3d6522da20ec5ccb71514d2afe160
Mohamed Mokhtar <82532768+Mokhtar628@users.noreply.github.com>
2022-04-24 14:38:41 +0200

    first Upload

---
+++ Controllers/AdminController.cs
@@ -0,0 +1,68 @@
+﻿using Online_Food_Ordering_System.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Online_Food_Ordering_System.Controllers
+{
+    public class AdminController : PersonController
+    {
+        private FoodyDatabaseEntities databaseEntityObject = new FoodyDatabaseEntities();
+
+
+        override
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        override
+        public ActionResult Login(User user)
+        {
+            if (ModelState.IsValid)
+            {
+                var returnedUserAfterCheckingDatabase = databaseEntityObject.Users.Where(userRecord => userRecord.Email.Equals(user.Email) && userRecord.pass.Equals(user.pass) && userRecord.Type.Equals("admin")).FirstOrDefault();
+                if (returnedUserAfterCheckingDatabase != null)
+                {
+                    OpenSessions(returnedUserAfterCheckingDatabase);
+                    return RedirectToAction("GoToHome");
+                }
+                else
+                {
+                    DisplayWrongDataError();
+                }
+            }
+            return View(user);
+        }
+
+        
+
+        override
+        public ActionResult GoToHome()
+        {
+            if (Session["ID"] != null)
+            {
+                return NavigateToAnthorView("~/Views/Admin/AdminHome.cshtml");
+            }
+            else
+            {
+                return RedirectToAction("Login");
+            }
+        }
+
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                databaseEntityObject.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Controllers/UsersController.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Online_Food_Ordering_System.Models;
+
+namespace Online_Food_Ordering_System.Controllers
+{
+    public class UsersController : PersonController
+    {
+        private FoodyDatabaseEntities databaseEntityObject = new FoodyDatabaseEntities();
+
+        
+        public ActionResult CreateUser()
+        {
+            return View();
+        }
+
+      
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult CreateUser([Bind(Include = "id,name,pass,Type,img,Email")] User user)
+        {
+            if (ModelState.IsValid)
+            {
+                user.Type = "user";
+                databaseEntityObject.Users.Add(user);
+                databaseEntityObject.SaveChanges();
+                return RedirectToAction("Login");
+            }
+
+            return View(user);
+        }
+
+        override
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        override
+        public ActionResult Login(User user)
+        {
+            if (ModelState.IsValid)
+            {
+                var returnedUserAfterCheckingDatabase = databaseEntityObject.Users.Where(userRecord => userRecord.Email.Equals(user.Email) && userRecord.pass.Equals(user.pass)).FirstOrDefault();
+                if (returnedUserAfterCheckingDatabase != null)
+                {
+                    OpenSessions(returnedUserAfterCheckingDatabase);
+                    return RedirectToAction("GoToHome");
+                }
+                else
+                {
+                    DisplayWrongDataError();
+                }
+            }
+            return View(user);
+        }
+
+        override
+        public ActionResult GoToHome()
+        {
+            if (Session["ID"] != null)
+            {
+                return NavigateToAnthorView("~/Views/Users/UserHome.cshtml");
+            }
+            else
+            {
+                return RedirectToAction("Login");
+            }
+        }
+
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                databaseEntityObject.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
