3c4bfed40aa5f779d021ca72699bc1ec9a5677df
HossamTourrato <61421680+HossamTourrato@users.noreply.github.com>
2022-04-30 01:20:16 +0200

    Adding new two classes
    adding show food class controller to show food in its categories and
    add get categories of food from database to use it in view

---
+++ Controllers/GetFoodCatecoriesController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Online_Food_Ordering_System.Models;
+
+namespace Online_Food_Ordering_System.Controllers
+{
+    public class GetFoodCatecoriesController : Controller
+    {
+        // GET: GetFoodCatecories
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public List<string> getFoodCategories(Category category)
+        {
+            List<string> foodCategories = new List<string>();
+            return foodCategories;
+        }
+    }
+}
---
+++ Controllers/ShowFoodController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Online_Food_Ordering_System.Controllers
+{
+    public class ShowFoodController : Controller
+    {
+        // GET: ShowFood
+        public ActionResult ShowFoodInCategory()
+        {
+            
+            return View();
+        }
+    }
+}
